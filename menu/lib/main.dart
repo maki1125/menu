0c287c5356483b5b54d88cdcb03643eba14cc968
工藤　真紀 <maki@kudoumakinoMacBook-Air.local>
2024-12-25 11:16:08 +0900

    ベース

--- menu/lib/main.dart
+++ menu/lib/main.dart
@@ -1,8 +1,9 @@
-//test_kudo
-//test_oku
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
