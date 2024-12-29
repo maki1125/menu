6a791b97be1cfdf8a101c3720f190842cdaf2fff
工藤　真紀 <maki@kudoumakinoMacBook-Air.local>
2024-12-29 14:32:35 +0900

    02_k_data_user

--- menu/lib/data/model/user.dart
+++ menu/lib/data/model/user.dart
@@ -1,9 +1,20 @@
-class User {
-  User({
+class UserModel {//UserにするとfirebaseAuthのUserと被るためUserModelとする。
+  UserModel({
     this.createAt,
-    this.uid
+    this.uid,
+    this.email,
   });
 
   DateTime? createAt;
   String? uid;
+  String? email;
+
+  //firebaseユーザからユーザモデルに変換
+  factory UserModel.fromFirebaseUser(firebaseuser) {
+    return UserModel(
+      uid: firebaseuser.uid,
+      email: firebaseuser.email ?? '',
+    );
+  }
+
 }
--- menu/lib/data/providers.dart
+++ menu/lib/data/providers.dart
@@ -1,3 +1,5 @@
+import 'package:menu/data/repository/user_repository.dart';
+
 import 'repository/menu_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'model/user.dart';
@@ -5,17 +7,22 @@ import 'model/menu.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-User user = User(
-            createAt: DateTime.now(), 
+//final userProvider = StateProvider<UserModel?>((ref) =>UserRepository().getCurrentUser());
+UserModel? currentUser = UserRepository().getCurrentUser();
+final userProvider = StateProvider<UserModel?>((ref) =>currentUser);
+/*
+            UserModel(
+            //createAt: DateTime.now(), 
             uid: "AC3iWb7RnqM4gCmeLOD9"
             );
+*/
 
 //final menuListProvider = StreamProvider<QuerySnapshot>((ref) {
 //  return MenuRepository(user).getMenuList();
 //});
 
 final menuListProvider = StreamProvider<List<Menu>>((ref) {
-  return MenuRepository(user).getMenuList();
+  return MenuRepository(currentUser!).getMenuList();
 });
 
 
