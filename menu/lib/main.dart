85c315d6054e3c5e57654e20bb94d7432b77e732
yosinori <155077578+YoshinoriOku@users.noreply.github.com>
2025-02-07 21:08:47 +0900

    o_17_remode

--- menu/lib/main.dart
+++ menu/lib/main.dart
@@ -3,13 +3,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_localizations/flutter_localizations.dart'; //日本語設定
 import 'package:intl/date_symbol_data_local.dart';
-
 import 'package:menu/login/view/login_screen.dart';
-
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await initializeDateFormatting('ja');
   runApp(
     const ProviderScope(child: MyApp()),
@@ -26,20 +25,18 @@ class MyApp extends ConsumerWidget {
 
     return const MaterialApp(
 
-      //日本語設定
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        Locale('ja'), // 日本語
-        Locale('en'), // 英語
-      ],
-      locale: Locale('ja'), // アプリのデフォルト言語を日本語に設定
-      
-      home: 
-       SignInAnony()
-    );
+        //日本語設定
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale('ja'), // 日本語
+          Locale('en'), // 英語
+        ],
+        locale: Locale('ja'), // アプリのデフォルト言語を日本語に設定
+
+        home: SignInAnony());
   }
 }
